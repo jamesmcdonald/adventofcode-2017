3d3062f77f7db8e0be53c5a6afb89842972ea4b0
James McDonald <james@redpill-linpro.com>
2017-12-13 08:45:58 +0100

    Overload severity calculation with a test mode

--- day13/firewall.go
+++ day13/firewall.go
@@ -10,15 +10,23 @@ import (
 
 var rules map[int]int
 
-func severity(rules map[int]int, delay int) int {
+// severity calculates the total severity of passing a firewall
+// if test is true, abort as soon as non-zero severity is encountered and return 1
+func severity(rules map[int]int, delay int, test bool) int {
 	severity := 0
 	for fdepth, frange := range rules {
 		fdepth += delay
 		if fdepth == 1 {
 			if frange == 1 {
+				if test {
+					return 1
+				}
 				severity++
 			}
 		} else if fdepth%((frange-1)*2) == 0 {
+			if test {
+				return 1
+			}
 			severity += fdepth * frange
 		}
 	}
@@ -48,9 +56,9 @@ func main() {
 		rules[fdepth] = frange
 	}
 
-	fmt.Println(severity(rules, 0))
+	fmt.Println(severity(rules, 0, false))
 	for d := 0; ; d++ {
-		if severity(rules, d) == 0 {
+		if severity(rules, d, true) == 0 {
 			fmt.Println("Delay", d)
 			break
 		}
