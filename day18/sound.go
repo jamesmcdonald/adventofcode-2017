625336694bb5ae61478aa804f8a6f271cf193945
James McDonald <james@redpill-linpro.com>
2017-12-20 16:25:44 +0100

    Day 18, only runs part 2

--- day18/sound.go
+++ day18/sound.go
@@ -12,23 +12,23 @@ import (
 
 type program struct {
 	id        int
-	reg       map[string]int64
-	last      int64
-	commands  map[int64][]string
-	output    chan int64
-	input     chan int64
+	reg       map[string]int
+	last      int
+	commands  map[int][]string
+	output    chan int
+	input     chan int
 	sendcount int
 }
 
-func (p program) valorreg(s string) int64 {
-	val, err := strconv.ParseInt(s, 10, 64)
+func (p program) valorreg(s string) int {
+	val, err := strconv.Atoi(s)
 	if err != nil {
 		return p.reg[s]
 	}
 	return val
 }
 
-func (p *program) cmd(parts []string) (int64, bool) {
+func (p *program) cmd(parts []string) (int, bool) {
 	switch parts[0] {
 	case "snd":
 		val := p.valorreg(parts[1])
@@ -49,20 +49,18 @@ func (p *program) cmd(parts []string) (int64, bool) {
 		p.reg[parts[1]] *= val
 		fmt.Printf("%d Multiply %s by %d [%d]\n", p.id, parts[1], val, p.reg[parts[1]])
 	case "rcv":
-		val := p.valorreg(parts[1])
-		if val != 0 {
-			c1 := make(chan bool)
-			go func() {
-				time.Sleep(time.Second * 1)
-				c1 <- true
-			}()
-			select {
-			case rcv := <-p.input:
-				fmt.Printf("%d Received last frequency: %d\n", p.id, rcv)
-			case <-c1:
-				fmt.Printf("%d Aborting on wait\n", p.id)
-				return 0, true
-			}
+		c1 := make(chan bool)
+		go func() {
+			time.Sleep(time.Second * 1)
+			c1 <- true
+		}()
+		select {
+		case rcv := <-p.input:
+			fmt.Printf("%d Received %d into %s\n", p.id, rcv, parts[1])
+			p.reg[parts[1]] = rcv
+		case <-c1:
+			fmt.Printf("%d Aborting on wait\n", p.id)
+			return 0, true
 		}
 	case "jgz":
 		cond := p.valorreg(parts[1])
@@ -81,22 +79,24 @@ func (p *program) cmd(parts []string) (int64, bool) {
 	return 1, false
 }
 
-func (p *program) runprogram(progreader io.Reader, id int, output chan int64, input chan int64) {
+func (p *program) runprogram(progreader io.Reader, id int, output chan int, input chan int) {
+	p.reg["p"] = id
 	p.id = id
-	p.commands = make(map[int64][]string)
+	p.commands = make(map[int][]string)
 	p.output = output
 	p.input = input
 	scanner := bufio.NewScanner(progreader)
-	var i int64
+	var i int
 	for scanner.Scan() {
 		p.commands[i] = strings.Split(scanner.Text(), " ")
 		i++
 	}
 	var length = i
 	fmt.Printf("%d Loaded %d instructions\n", id, length)
-	pc := int64(0)
+	pc := 0
 	for {
 		if pc < 0 || pc >= length {
+			fmt.Printf("%d Stepping out of program, stop\n", p.id)
 			break
 		}
 		jump, stop := p.cmd(p.commands[pc])
@@ -116,10 +116,10 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	p0 := program{reg: make(map[string]int64)}
-	p1 := program{reg: make(map[string]int64)}
-	fromzero := make(chan int64, 1000)
-	fromone := make(chan int64, 1000)
+	p0 := program{reg: make(map[string]int)}
+	p1 := program{reg: make(map[string]int)}
+	fromzero := make(chan int, 1000)
+	fromone := make(chan int, 1000)
 	go p0.runprogram(input0, 0, fromzero, fromone)
 	p1.runprogram(input1, 1, fromone, fromzero)
 	fmt.Println(p0.sendcount, p1.sendcount)
